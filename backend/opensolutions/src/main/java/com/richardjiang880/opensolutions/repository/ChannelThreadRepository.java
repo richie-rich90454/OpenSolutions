9326e664e09b3d87bb2a82a4ad4d1eff452fd6eb
richie-rich90454 <rjiang880@gmail.com>
2026-06-12 12:33:00 +0800

    feat: add ChannelThreadRepository for channel threads

---
+++ backend/opensolutions/src/main/java/com/richardjiang880/opensolutions/repository/ChannelThreadRepository.java
@@ -0,0 +1,13 @@
+package com.richardjiang880.opensolutions.repository;
+
+import com.richardjiang880.opensolutions.model.ChannelThread;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChannelThreadRepository extends JpaRepository<ChannelThread, Long> {
+
+    Page<ChannelThread> findByChannelIdOrderByCreatedAtDesc(Long channelId, Pageable pageable);
+}
